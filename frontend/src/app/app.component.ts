017c208b3bb32478abe0bb177d30bafa1e64bf02
BaptisteRaux <baptiste.raux.br@gmail.com>
2024-12-27 16:02:39 +0100

    Initial commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgbDropdownToggle, NgbDropdown, NgbDropdownMenu, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'TD2';
+}
---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
+import {Routes} from '@angular/router';
+
+export const routes: Routes = [
+  { path: 'todos', component: TodoListPageComponent },
+  // Ajoutez d'autres routes si nécessaire
+];
