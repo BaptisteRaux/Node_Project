49e7e5803e4ecb8f828833446a20246d258a80e9
LaELiance <eliasnt04@hotmail.com>
2024-12-29 17:44:50 +0100

    change access to movies

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';
+import cors from '../frontend/node_modules/cors';
 import filmRoutes from '../routes/filmRoutes';
 import userRoutes from '../routes/userRoutes';
 import swaggerUi from 'swagger-ui-express';
